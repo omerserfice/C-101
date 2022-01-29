08f9150ad58e2212d8024ef6f2a4c8b7755a7f4c
omerserfice <srfcomr@gmail.com>
2022-01-29 15:30:11 +0300

    degiskenler konusu

--- Program.cs
+++ Program.cs
@@ -13,6 +13,33 @@ namespace console_programlama
             Console.WriteLine("soyadını gir:");
             string surname = Console.ReadLine();
             Console.WriteLine("Merhaba"+name+" "+surname);
-                  }
+            // degişkenler 
+
+             int deger = 2;
+            string degisken = null;
+            string Degisken = null;
+           
+           byte b = 5;
+           sbyte c = 6;
+
+           short s = 5;
+            
+            Console.WriteLine(deger+degisken);
+            DateTime dt = DateTime.Now;
+            System.Console.WriteLine(dt);
+
+            object o1 = "x";
+            object o2 = 4;
+            // string ifadeler
+
+            string str1 = string.Empty;
+            str1 = "Ömer Serfice";
+        
+             int d2 = -2147483648;
+             byte d3 = 255;
+             sbyte  d4 = -128;
+             
+    
+    }
     }
 }
