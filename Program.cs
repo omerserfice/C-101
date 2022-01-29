aa4f8405137ca5a5b021788767d830850b0d1c59
omerserfice <srfcomr@gmail.com>
2022-01-29 13:52:09 +0300

    c# ödevleri

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace console_programlama
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("İsmini gir:");
+            string name = Console.ReadLine();
+            Console.WriteLine("soyadını gir:");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Merhaba"+name+" "+surname);
+                  }
+    }
+}
